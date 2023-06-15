76d6388eb9ef375b3b0741292d84f81d8fcabc75
LAPTOP-58QGQQLE\David Private <jonid89@gmail.com>
2023-06-15 19:51:58 +0200

    myButtons null reference fix + AppInstaller InitExecutionOrder

--- Assets/Scripts/AppInstaller.cs
+++ Assets/Scripts/AppInstaller.cs
@@ -9,6 +9,12 @@ public class AppInstaller : MonoInstaller
     [SerializeField] private GameObject _buttonView;
     [SerializeField] private ButtonsSettings buttonsSettings; 
 
+    void InitExecutionOrder()
+    {
+        Container.BindExecutionOrder<ButtonsSettings>(-10);
+        Container.BindExecutionOrder<ObjectSpawner>(-20);
+    }
+
     public override void InstallBindings()
     {        
         //Container.Bind<ButtonGrid>().AsSingle().NonLazy();
